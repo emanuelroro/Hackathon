7cba7cc5ee8b187bc7b3a1226ee60c2a1e25b702
unknown <student@cs.colman.ac.il>
2019-02-27 22:22:19 +0200

    added tags insert

--- ServerSide/index.js
+++ ServerSide/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 const firebase = require("firebase");
 var config = {
@@ -12,15 +13,35 @@ var config = {
 firebase.initializeApp(config);
 const port = 3000
 const db = firebase.firestore();
+
 //app.get('/', (req, res) => {
-//
+ // res.send("hello world");
 //})
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.post('/event', (req, res) => {
-    const event = req.body.event;
+    const event = req.body;
+    addTags(event.tags)
     let addDoc = db.collection('events').add(event).then(ref => {
         console.log('Added document with ID: ', ref.id);
-      });
+    });
+    res.send();
 })
+
+function addTags(tagsToAdd) {
+    tagsToAdd.forEach((tag) => {
+        let newAmount = 1;
+        const tagsRef = db.collection('tags').doc(tag);
+        const getDoc = tagsRef.get()
+        .then(doc => {
+          
+        if (doc.exists) {
+          newAmount += doc.data().amount;   
+        }
+        tagsRef.set({amount: newAmount});
+        })
+    })
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
