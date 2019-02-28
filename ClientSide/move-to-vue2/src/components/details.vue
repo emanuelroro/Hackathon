4fba7a20fb94ba187eb1eb059bc11b0e574e4c7d
unknown <student@cs.colman.ac.il>
2019-02-28 04:50:45 +0200

    C:/Program Files/Git

---
+++ ClientSide/move-to-vue2/src/components/details.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <event-Details v-for="det in detail" :det="det" class=""></event-Details>
+</div>
+    </template>
+
+<script>
+import eventDetails from "./eventDetails.vue";
+export default {
+  name: 'detailss',
+  components: {
+    "event-Details": eventDetails,
+    },
+  data() {
+      return {
+          detail: [
+              {
+                description: 'im in tourist class ans want to upgrade to bussines class',
+                email: 'jdshf@jfdksjlf.com',
+                price: 300,
+                tags: ['flight', 'seat', 'upgrade'],
+                title: 'Want bussines class ticket in the flight',
+                upgrade: false
+              },
+              {
+                description: 'line 3 seat 4',
+                email: 'jddsfsshf@jfdksjlf.com',
+                price: 200,
+                tags: ['show', 'omer-adam', 'fun', 'king', 'mizrahit', 'male-cesef', ''],
+                title: 'Show of omer adam- where is all your female friens',
+                upgrade: true
+              }
+          ]
+      }
+  }
+    
+  }
+
+</script>
---
+++ ClientSide/move-to-vue2/src/components/eventDetails.vue
@@ -0,0 +1,37 @@
+<template>
+        <div class="box" dir="ltr" >
+                <article class="media">
+                  <div class="media-content">
+                    <div class="content">
+                      <p>
+                        <strong>{{this.det.title}}</strong>
+                        <br>
+                        {{this.det.description}}
+                        <br>
+                        <div class="tags">
+                                <span class="tag" v-for="tag in this.det.tags">{{tag}}</span>
+                              </div>
+                      </p>
+                    </div>
+                    <nav class="level is-mobile">
+                      <div class="level-left">
+                      {{this.det.email}}
+                      </div>
+                      <div class="level-right">
+                            <span style="font-size: 130%; margin-right: 8%" >{{this.det.price}}$</span>
+                            <a class="button is-success is-rounded" v-if="this.det.upgrade">choose</a>
+                            <a class="button is-warning is-rounded" v-if="!this.det.upgrade">choose</a>
+                            
+                    </div>
+                    </nav>
+                  </div>
+                </article>
+              </div>
+</template>
+
+<script>
+        export default {
+          name: 'eventDetails',
+          props: ['det'],
+        }
+</script>
